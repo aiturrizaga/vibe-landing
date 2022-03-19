6eb0738f9261d5dd4054ce43d3ac392b2880d53c
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2022-03-18 23:09:35 -0500

    Correciones

--- src/app/shared/services/constant.service.ts
+++ src/app/shared/services/constant.service.ts
@@ -7,7 +7,7 @@ export class ConstantService {
 
   facebookLink = 'https://www.facebook.com/eresvibe';
   instagramLink = 'https://www.instagram.com/eresvibe';
-  tiktokLink = 'https://www.instagram.com/eresvibe';
+  tiktokLink = 'https://vm.tiktok.com/ZMLa22PcG/';
   youtubeLink = 'https://www.youtube.com/channel/UC241VtTgCoGTudqPSu0BpFQ';
   waLink = 'https://api.whatsapp.com/send?phone=51901801803&text=Hola%2C%20quiero%20saber%20m%C3%A1s%20acerca%20de%20VIBE%20%F0%9F%98%8A';
 
