6322b39359932993370c4ccf4e7016a6eb3e9740
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2022-03-18 21:12:14 -0500

    About us page mobile ready

--- src/app/public/pages/about-us/about-us.page.ts
+++ src/app/public/pages/about-us/about-us.page.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import SwiperCore, { Autoplay, Navigation, SwiperOptions } from "swiper";
+
+SwiperCore.use([Autoplay, Navigation]);
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.page.html',
-  styles: [
-  ]
+  styles: []
 })
 export class AboutUsPage implements OnInit {
 
-  constructor() { }
+  artistCarouselConfig: SwiperOptions = {
+    loop: false,
+    slidesPerView: 'auto',
+    spaceBetween: 100,
+    allowTouchMove: true,
+    navigation: false,
+    autoplay: {
+      delay: 6000,
+      disableOnInteraction: false
+    }
+  };
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     window.scroll(0, 0);
